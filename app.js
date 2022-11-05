3d82f9c4d47ab78ac03ddba710f026021c75a8a9
StefanPacola <stevo.pacola@gmail.com>
2022-11-05 17:32:41 +0100

    ADD: interval on input

--- app.js
+++ app.js
@@ -26,53 +26,55 @@ async function fetchData(city){
 };
 
 // INPUT:
-inputPlace.addEventListener("keypress", async (e) => {
-    
-    if(e.key === "Enter" && e.target.value !== ""){
-        let placeInfo = await fetchData(e.target.value);
-        // console.log(placeInfo);
+function onEnter(){
+    // alert("2,5s")
+    inputPlace.addEventListener("keypress", async (e) => {
+        
+        if(e.key === "Enter" && e.target.value !== ""){
+            let placeInfo = await fetchData(e.target.value);
+            // console.log(placeInfo);
 
-        // Zmeny v html:
+            // Zmeny v html:
 
-        // všeobecné info o počasí
-        descIcon.src = `https://openweathermap.org/img/wn/${placeInfo.weather["0"].icon}@2x.png`;
-        descText.textContent = placeInfo.weather["0"].description;
+            // všeobecné info o počasí
+            descIcon.src = `https://openweathermap.org/img/wn/${placeInfo.weather["0"].icon}@2x.png`;
+            descText.textContent = placeInfo.weather["0"].description;
 
-        // dáta o počasí:
-        h2.textContent = `Počasie pre lokalitu ${placeInfo.name}`;
-        h2.style.fontSize = "1.5rem";
-        temp.textContent = `${placeInfo.main.temp}°C`;
-        maxTemp.textContent = `Maximálna teplota: ${placeInfo.main.temp_max}°C`;
-        minTemp.textContent = `Minimálna teplota: ${placeInfo.main.temp_min}°C`;
-        feelsLike.textContent = `Pocitová teplota: ${placeInfo.main.feels_like}°C`;
-        humidity.textContent = `Vlhkosť vzduchu: ${placeInfo.main.humidity}%`;
-        pressure.textContent = `Tlak vzduchu: ${placeInfo.main.pressure} hPa`;
-        windSpeed.textContent = `Rýchosť vetra: ${placeInfo.wind.speed} km/h`;
-
-        // zrážky, ak sú dáta dostupné:
-        if(placeInfo.rain || placeInfo.snow){
-            rain.textContent = `Zrážky poslednú hodinu: ${placeInfo.rain["1h"]} mm`;
-            snow.textContent = `Snehové zrážky za poslednú hodinu ${placeInfo.snow["1h"]}`;
+            // dáta o počasí:
+            h2.textContent = `Počasie pre lokalitu ${placeInfo.name}`;
+            h2.style.fontSize = "1.5rem";
+            temp.textContent = `${placeInfo.main.temp}°C`;
+            maxTemp.textContent = `Maximálna teplota: ${placeInfo.main.temp_max}°C`;
+            minTemp.textContent = `Minimálna teplota: ${placeInfo.main.temp_min}°C`;
+            feelsLike.textContent = `Pocitová teplota: ${placeInfo.main.feels_like}°C`;
+            humidity.textContent = `Vlhkosť vzduchu: ${placeInfo.main.humidity}%`;
+            pressure.textContent = `Tlak vzduchu: ${placeInfo.main.pressure} hPa`;
+            windSpeed.textContent = `Rýchosť vetra: ${placeInfo.wind.speed} km/h`;
+            
+            // zrážky, ak sú dáta dostupné:
+            if(placeInfo.rain || placeInfo.snow){
+                rain.textContent = `Zrážky poslednú za hodinu: ${placeInfo.rain["1h"]} mm`;
+                snow.textContent = `Snehové zrážky za poslednú hodinu ${placeInfo.snow["1h"]}`;
+            }
         }
-        return
-    }
-    else{
-        descIcon.src = "";
-        descText.textContent = "";
+        else{
+            descIcon.src = "";
+            descText.textContent = "";
 
-        h2.textContent = "Lokalita nebola nájdená.";
-        h2.style.fontSize = "1.5rem";
-        temp.textContent = "";
-        maxTemp.textContent = "";
-        minTemp.textContent = "";
-        feelsLike.textContent = "";
-        humidity.textContent = "";
-        pressure.textContent = "";
-        windSpeed.textContent = "";
-        rain.textContent = "";
-        snow.textContent = "";
-    }
-});
+            h2.textContent = "Lokalita nebola nájdená.";
+            h2.style.fontSize = "1.5rem";
+            temp.textContent = "";
+            maxTemp.textContent = "";
+            minTemp.textContent = "";
+            feelsLike.textContent = "";
+            humidity.textContent = "";
+            pressure.textContent = "";
+            windSpeed.textContent = "";
+            rain.textContent = "";
+            snow.textContent = "";
+        }
+    })
+};
 
 // BUTTON
 btn.addEventListener("click", async (e) => {
